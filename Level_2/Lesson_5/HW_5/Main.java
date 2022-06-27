22dc3ec53c174d57074420a8bd80805078b1e3ee
Platonov Roman <romaa.6633@gmail.com>
2022-06-27 18:57:39 +0400

    Changed HW-5

--- Level_2/Lesson_5/HW_5/Main.java
+++ Level_2/Lesson_5/HW_5/Main.java
@@ -12,15 +12,11 @@ public class Main {
 
         firstMethod(array);
         secondMethod(array);
-
     }
 
     public static void firstMethod(float[] array) {
         long a = System.currentTimeMillis();
-        for (int i = 0; i < array.length; i++) {
-            array[i] = (float) (array[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) *
-                    Math.cos(0.4f + i / 2));
-        }
+        setValue(array, 0f);
         System.currentTimeMillis();
         System.out.println("One thread time: " + (System.currentTimeMillis() - a) + " mc.");
     }
@@ -33,15 +29,17 @@ public class Main {
         System.arraycopy(array, 0, leftHalf, 0, HALF);
         System.arraycopy(array, HALF, rightHalf, 0, HALF);
 
-        Thread thread1 = new Thread(() -> {
-            setValue(leftHalf);
-        });
-
-        Thread thread2 = new Thread(() -> {
-            setValue(rightHalf);
-        });
+        Thread thread1 = new Thread(() -> setValue(leftHalf, 0f));
+        Thread thread2 = new Thread(() -> setValue(rightHalf, rightHalf[0]));
         thread1.start();
         thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
         float[] mergedArray = new float[SIZE];
         System.arraycopy(leftHalf, 0, mergedArray, 0, HALF);
@@ -49,16 +47,14 @@ public class Main {
 
         System.currentTimeMillis();
         System.out.println("Two thread time: " + (System.currentTimeMillis() - a) + " mc.");
-
     }
 
-    public static void setValue(float[] array) {
+    public static void setValue(float[] array, float number) {
         for (int i = 0; i < array.length; i++) {
-            array[i] = (float) (array[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) *
-                    Math.cos(0.4f + i / 2));
+            array[i] = (float) (array[i] * Math.sin(0.2f + (i + number) / 5) * Math.cos(0.2f + (i + number) / 5) *
+                    Math.cos(0.4f + (i + number) / 2));
         }
     }
-
 }
 
 
